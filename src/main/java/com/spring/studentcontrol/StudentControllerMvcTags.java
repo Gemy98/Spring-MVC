d82a134e27f8bc0a80bc65d8dde544a0c1369e2d
Gemy98 <mohamedgamal19981998@gmail.com>
2023-04-13 21:28:03 +0200

    Space Validation - @InitBinder function( remove spaces )

--- src/main/java/com/spring/studentcontrol/StudentControllerMvcTags.java
+++ src/main/java/com/spring/studentcontrol/StudentControllerMvcTags.java
@@ -1,8 +1,11 @@
 package com.spring.studentcontrol;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -35,5 +38,12 @@ public class StudentControllerMvcTags {
 		return "student-front-mvc-tags/showdatastudent";
 		}
 	}
+	
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		StringTrimmerEditor editor =new StringTrimmerEditor(true);
+		binder.registerCustomEditor(String.class,editor);
+	}
+	
 
 }
